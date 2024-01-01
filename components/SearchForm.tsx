e972974a04897da57be71448df90b9a85d459d87
Jozef S <franck.vukelic@gmail.com>
2024-01-01 20:06:18 +0700

    deploy

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,11 +1,38 @@
 "use client";
-
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
 import Image from "next/image";
 import { Input } from "@/components/ui/input";
+import { formUrlQuery } from "@/sanity/utils";
 
 const SearchForm = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
   const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      let newUrl = "";
+
+      if (search) {
+        newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "query",
+          value: search,
+        });
+      } else {
+        newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          keysToRemove: ["query"],
+        });
+      }
+      router.push(newUrl, { scroll: false });
+    }, 300);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [search]);
 
   return (
     <form className="flex-center mx-auto mt-10 w-full sm:-mt-10 sm:px-5">
@@ -19,7 +46,7 @@ const SearchForm = () => {
         />
         <Input
           className="base-regular h-fit border-0 bg-black-400 py-6 pl-20 pr-8 text-white-800 !ring-0 !ring-offset-0 placeholder:text-white-800"
-          type="text"          
+          type="text"
           placeholder="Search"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
